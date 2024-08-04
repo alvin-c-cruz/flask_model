22fc531a3375c33720e9fada9f04aa967e7ba83f
alvin-c-cruz <alvinccruz12@gmail.com>
2024-08-05 04:01:03 +0800

    Added handling of Date Field

--- application/blueprints/disbursement/models.py
+++ application/blueprints/disbursement/models.py
@@ -3,6 +3,7 @@ from application.extensions import db
 
 class Disbursement(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
+    record_date = db.Column(db.Date())
     
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref='disbursements', lazy=True)
